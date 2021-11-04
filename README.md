df60aeb38c68be3ea7f3d21cc8e1b101f02bf7e2
Markus Walther <mark@saiko.tech>
2021-11-04 21:51:15 +0000

    GCPBillingCap component

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # gcp-billing-capper
+
+## Usage
+
+```py
+cloudresourcemanager_enable = projects.Service(
+    'cloudresourcemanager-api',
+    service='cloudresourcemanager.googleapis.com')
+
+billingbudgets_enable = projects.Service(
+    'billingbudgets-api',
+    service='billingbudgets.googleapis.com')
+
+shutil.make_archive('/tmp/capper', 'zip', 'capper')
+
+billing_project = gcp.organizations.get_project().name
+billing_account = organizations.get_billing_account(billing_account=config.require('gcpBillingAccount'))
+
+capper.GCPBillingCap(
+    'gcp-billing-cap',
+    args=capper.GCPBillingCapArgs(
+        billing_account=billing_account.id,
+        billing_project=billing_project,
+        currency_code=config.require('gcpCurrencyCode'),
+        max_spend=config.require('gcpMaxSpend'),
+        capper_zip_path='/tmp/capper.zip'),
+    opts=ResourceOptions(depends_on=[billingbudgets_enable, cloudresourcemanager_enable]))
+```
---
+++ pulumi.py
@@ -0,0 +1,121 @@
+from typing import Optional
+import pulumi
+from pulumi.output import Input, Output
+import pulumi_gcp
+import pulumi_random
+from pulumi.resource import ResourceOptions
+from pulumi_gcp import billing, pubsub, serviceaccount, cloudfunctions, storage
+
+
+class GCPBillingCapArgs:
+
+    billing_account: Input[str]
+    billing_project: Input[str]
+    currency_code: Input[str]
+    max_spend: Input[str]
+    capper_zip_path: str
+
+#    @staticmethod
+#    def from_inputs(inputs: Inputs) -> 'GCPBillingCapArgs':
+#        return GCPBillingCapArgs(
+#            billing_account=inputs['billingAccount'],
+#            billing_project=inputs['billingProject'],
+#            currency_code=inputs['currencyCode'],
+#            max_spend=inputs['maxSpend'])
+
+    def __init__(
+            self,
+            billing_account: Input[str],
+            billing_project: Input[str],
+            currency_code: Input[str],
+            max_spend: Input[str],
+            capper_zip_path: str) -> None:
+        self.billing_account = billing_account
+        self.billing_project = billing_project
+        self.currency_code = currency_code
+        self.max_spend = max_spend
+        self.capper_zip_path = capper_zip_path
+
+
+class GCPBillingCap(pulumi.ComponentResource):
+    def __init__(
+            self,
+            name: str,
+            args: GCPBillingCapArgs,
+            props: Optional[dict] = None,
+            opts: ResourceOptions = None):
+        super().__init__('pkg:index:GCPBillingCap', name, None, opts)
+
+        topic = pubsub.Topic(
+            'billing-alerts',
+            name='billing-capper-alerts',
+            opts=ResourceOptions(parent=self))
+
+        bucket = storage.Bucket(
+            'cloudfunctions-source',
+            name='billing-capper-cloudfunction-source',
+            opts=ResourceOptions(parent=self))
+
+        archive = storage.BucketObject(
+            'cloudfunctions-source',
+            bucket=bucket.name,
+            source=pulumi.FileAsset(args.capper_zip_path),
+            opts=ResourceOptions(parent=self))
+
+        function = cloudfunctions.Function(
+            'capper',
+            name='billing-capper',
+            region='europe-west1', # TODO: make this configurable
+            runtime='python39',
+            available_memory_mb=128,
+            source_archive_bucket=bucket.name,
+            source_archive_object=archive.name,
+            event_trigger=cloudfunctions.FunctionEventTriggerArgs(
+                event_type='google.pubsub.topic.publish',
+                resource=topic.id),
+            entry_point='stop_billing',
+            opts=ResourceOptions(parent=self))
+
+        suffix = pulumi_random.RandomString(
+            'account-id-suffix',
+            length=3,
+            min_lower=3,
+            opts=ResourceOptions(parent=self))
+
+        pubsub_invoker_sa = serviceaccount.Account(
+            'pubsub-push-to-capper',
+            account_id=Output.concat('pubsub-push-to-capper', suffix.result),
+            display_name=f'Pub/Sub Push to capper Invoker',
+            opts=ResourceOptions(parent=self))
+
+        perms = cloudfunctions.FunctionIamMember(
+            'pubsub-to-capper-invoker',
+            project=function.project,
+            region=function.region,
+            cloud_function=function.name,
+            role='roles/cloudfunctions.invoker',
+            member=pubsub_invoker_sa.email.apply(lambda email: f'serviceAccount:{email}'),
+            opts=ResourceOptions(parent=self))
+
+        billing_provider = pulumi_gcp.Provider(
+            'billing-provider',
+            billing_project=args.billing_project,
+            user_project_override=True,
+            opts=ResourceOptions(parent=self))
+
+        billing.Budget(
+            'budget',
+            billing_account=args.billing_account,
+            display_name='Billing Capper Budget',
+            amount=billing.BudgetAmountArgs(
+                specified_amount=billing.BudgetAmountSpecifiedAmountArgs(
+                    currency_code=args.currency_code,
+                    units=args.max_spend)),
+            threshold_rules=[billing.BudgetThresholdRuleArgs(
+                threshold_percent=0.5)],
+            all_updates_rule=billing.BudgetAllUpdatesRuleArgs(
+                pubsub_topic=topic.id),
+            opts=ResourceOptions(
+                parent=self,
+                depends_on=[perms],
+                provider=billing_provider))
