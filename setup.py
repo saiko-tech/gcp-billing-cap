42df7a845df89b1a27423f4f981da5b25de90e61
Markus Walther <mark@saiko.tech>
2021-11-04 23:07:50 +0000

    fix layout a bit

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'gcp-billing-cap',
+    name = 'gcp_billing_cap',
     version = '0.1.0',
     url = 'https://github.com/saiko-tech/gcp-billing-cap',
     description = 'Pulumi package for capping GCP Billing vi Pub/Sub & Cloud Functions',
-    packages = find_packages()
-)
+    packages = find_packages(where='pulumi'),
+    package_dir={'gcp_billing_cap': './pulumi/gcp_billing_cap'})
