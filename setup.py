260924ed0a7fab60ed3605a57a59cd67f814c5b3
Markus Walther <mark@saiko.tech>
2022-02-12 15:24:18 +0000

    rename from gcp-billing-cap to pulumi-gcp-billing-cap

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name = 'gcp-billing-cap',
+    name = 'pulumi-gcp-billing-cap',
     version = '0.1.0',
     url = 'https://github.com/saiko-tech/gcp-billing-cap',
     description = 'Pulumi package for capping GCP Billing vi Pub/Sub & Cloud Functions',
-    packages = ['gcp_billing_cap'],
+    packages = ['pulumi_gcp_billing_cap'],
     package_dir={
-        'gcp_billing_cap': './pulumi/gcp_billing_cap'
+        'pulumi_gcp_billing_cap': './pulumi/gcp_billing_cap'
     },
     install_requires=[
         'pulumi>=3.0.0,<4.0.0',
